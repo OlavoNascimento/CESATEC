b3778d6ff13e5d1dad38cb44460b1b7ddbe1508e
Olavo Nascimento <olavo.nascimento4@gmail.com>
2018-10-15 15:04:37 -0300

    Mover a imagem do estudante na atividade de detalhes para a barra de menu

--- app/src/main/java/cesatec/cesatec/fragments/StudentDetailFragment.java
+++ app/src/main/java/cesatec/cesatec/fragments/StudentDetailFragment.java
@@ -2,10 +2,14 @@ package cesatec.cesatec.fragments;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,49 +20,64 @@ import cesatec.cesatec.R;
 public class StudentDetailFragment extends Fragment {
     static final String TAG = "StudentDetailFragment";
 
+    private int id;
+    private String name;
+    private String avatarUrl;
+
+    /**
+     * Set the fragment variables used the arguments passed to the fragment
+     *
+     * @param savedInstanceState Saved fragmented data to be reused
+     */
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Bundle arguments = getArguments();
         if (arguments != null) {
-            short id = getArguments().getShort("id");
-            String name = getArguments().getString("name");
-            String avatarUrl = getArguments().getString("avatar_url");
-            setWidgets(id, name, avatarUrl);
+            Log.d(TAG, "onCreate: Student detail " + arguments.toString());
+            this.id = getArguments().getShort("id");
+            this.name = getArguments().getString("name");
+            this.avatarUrl = getArguments().getString("avatar_url");
         }
     }
 
     /**
-     * Set the information of the user on the UI
-     *
-     * @param id        Id of the student
-     * @param name      Name of the student
-     * @param avatarUrl Url of the avatar of the student
+     * Set up widgets after the fragment views are created and available
      */
-    private void setWidgets(Short id, String name, String avatarUrl) {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        setWidgets();
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+    /**
+     * Set the information of the user on the UI
+     */
+    private void setWidgets() {
         Log.d(TAG, "setWidgets: setting Student information on detail activity");
 
         Activity activity = getActivity();
-
         if (activity != null) {
-            TextView idView = activity.findViewById(R.id.detail_id);
-            TextView nameView = activity.findViewById(R.id.detail_name);
-            ImageView avatarView = activity.findViewById(R.id.detail_avatar);
-
             CollapsingToolbarLayout appBarLayout = activity.findViewById(R.id.toolbar_layout);
             appBarLayout.setTitle(name);
 
-            String idText = getString(R.string.detail_id, id);
-            String nameText = getString(R.string.detail_name, name);
+            TextView idView = activity.findViewById(R.id.student_id);
+            String idText = getString(R.string.student_detail_id, id);
             idView.setText(idText);
+
+            TextView nameView = activity.findViewById(R.id.student_name);
+            String nameText = getString(R.string.student_detail_name, name);
             nameView.setText(nameText);
+
+            ImageView avatarView = activity.findViewById(R.id.student_avatar);
             Picasso.get()
                     .load(avatarUrl)
                     .placeholder(R.mipmap.ic_launcher_round)
                     .into(avatarView);
         }
     }
-
-
 }
